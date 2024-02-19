d4a6d722ea835be064d3bd77ce150e5e8b20d4df
4epexa <57490820+4epexa@users.noreply.github.com>
2024-02-19 19:21:16 +0300

    Create setLeadId.js

---
+++ setLeadId.js
@@ -0,0 +1,25 @@
+function getInputData(name) {
+	let myObject = document.querySelectorAll(name);
+	let valuesArray = Object.values(myObject).map(function(obj) {
+		return obj.value;
+	});
+	return valuesArray.find(function(value) {
+		return value !== "";
+	});
+}
+
+
+function setCookie() {
+	let phone = getInputData('input[data-tilda-rule="phone"]')
+	let time = Date.now()
+	let hash = CryptoJS.MD5(phone + time);
+	document.cookie = "lead_id=" + hash + "; path=/; domain=." + location.hostname.replace(/^www\./i, "")
+}
+
+document.addEventListener('submit', function(event) {
+	if (event.event && event.event.startsWith('submit_form')) {
+		setCookie();
+	}
+	
+	console.log("Cookie lead_id установлена" );
+});
